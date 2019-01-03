61bd4ac7de2454c4bc09f14045e0da3468ac652f
JM Dev <bat>
2019-01-03 04:35:21 -0500

    updates

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -56,7 +56,7 @@ export default class Home extends Component {
   renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
+        <h1>Jmdev - Notes</h1>
         <p>A simple note taking app</p>
         <div>
           <Link to="/login" className="btn btn-info btn-lg">
